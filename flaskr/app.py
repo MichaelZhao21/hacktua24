0889eec18787de639861e4006c167a481d22eda3
Michael Zhao <michaelzhao314@gmail.com>
2024-10-13 03:30:21 -0500

    This doesn't work but sachi needs it right now

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template, request
 from src.download import download_video
+from src.extract import extract_notes
 
 app = Flask(__name__)
 
@@ -9,10 +10,26 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/download", methods=["POST"])
-def download():
+@app.route("/snatch", methods=["POST"])
+def snatch():
+    # Download the video
     url = request.form["url"]
-    return download_video(url)
+    name = download_video(url)
+
+    # Extract the notes
+    notes = extract_notes(name)
+    print(notes)
+
+    return name
+
+    
+@app.post("/download")
+def download():
+    # Download the video
+    url = request.form["url"]
+    name = download_video(url)
+
+    return name
 
 
 if __name__ == "__main__":
