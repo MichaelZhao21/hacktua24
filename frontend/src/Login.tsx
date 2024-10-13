eb976fa50f03a912f7e347980ba578c349a7e2be
Michael Zhao <michaelzhao314@gmail.com>
2024-10-13 06:08:59 -0500

    Fixed issues with login

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,17 +1,16 @@
-import * as React from 'react';
-import { AuthProvider, AppProvider, SignInPage } from '@toolpad/core';
-import { useTheme } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
+import { AuthProvider, AppProvider, SignInPage } from "@toolpad/core";
+import { useTheme } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 // preview-start
 const providers: AuthProvider[] = [
-  { id: 'github' as 'github', name: 'GitHub' },
-  { id: 'google' as 'google', name: 'Google' },
-  { id: 'facebook' as 'facebook', name: 'Facebook' },
-  { id: 'twitter' as 'twitter', name: 'Twitter' },
-  { id: 'linkedin' as 'linkedin', name: 'LinkedIn' },
+  { id: "github" as "github", name: "GitHub" },
+  { id: "google" as "google", name: "Google" },
+  { id: "facebook" as "facebook", name: "Facebook" },
+  { id: "twitter" as "twitter", name: "Twitter" },
+  { id: "linkedin" as "linkedin", name: "LinkedIn" },
 ];
-import './Split.css';
+import "./Split.css";
 
 const signIn: (provider: AuthProvider) => void = async (provider) => {
   const promise = new Promise<void>((resolve) => {
@@ -28,22 +27,26 @@ export default function Login() {
   return (
     <div className="poppins-bold">
       <AppProvider theme={theme}>
-      <div className="flex flex-col items-center justify-center min-h-screen h-screen split-background"> {/* Background and flexbox for centering */}
-        <SignInPage
-          signIn={signIn}
-          providers={providers}
-          className="bg-white shadow-md rounded-lg p-6" // Styling for the SignInPage component
-        />
-      </div>
+        <div className="flex flex-col items-center justify-center min-h-screen h-screen split-background">
+          {" "}
+          {/* Background and flexbox for centering */}
+          <SignInPage
+            signIn={signIn}
+            providers={providers}
+            className="bg-white shadow-md rounded-lg p-6" // Styling for the SignInPage component
+          />
+        </div>
       </AppProvider>
-    <div>
-    <Button>
-        <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>Hack Tua</Link>
-    </Button>
-    <AppProvider theme={theme}>
-      <SignInPage signIn={signIn} providers={providers} />
-    </AppProvider>
-
+      <div>
+        <Button>
+          <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+            Hack Tua
+          </Link>
+        </Button>
+        <AppProvider theme={theme}>
+          <SignInPage signIn={signIn} providers={providers} />
+        </AppProvider>
+      </div>
     </div>
   );
 }
