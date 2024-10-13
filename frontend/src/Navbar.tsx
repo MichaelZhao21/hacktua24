b5d4bef0f31c3871feee2260411b843e81858185
Michael Zhao <michaelzhao314@gmail.com>
2024-10-13 11:48:00 -0500

    Added xmls and navbar shi

--- frontend/src/Navbar.tsx
+++ frontend/src/Navbar.tsx
@@ -1,13 +1,12 @@
-
 import { Button } from "@mui/material";
 
 import { Link } from "react-router-dom";
-import logo from './logo.png';
-
+import logo from "./logo.png";
 
 function Navbar() {
-    return (
-      <div className="fixed top-0 left-0 bg-blue-100 rounded-b-lg drop-shadow-md w-full p-2 flex flex-row pr-6 items-center">
+  return (
+    <div className="fixed top-0 left-0 bg-blue-100 rounded-b-lg drop-shadow-md w-full p-2 flex flex-row pr-6 items-center">
+      <a className="grow flex flex-row items-center cursor-pointer" href="/" >
         <div
           style={{ textAlign: "center", padding: "5px", paddingLeft: "20px" }}
         >
@@ -17,23 +16,26 @@ function Navbar() {
             style={{ width: "30px", height: "auto" }}
           />
         </div>
-        <div className="poppins-bold grow ml-2 text-xl text-primary">ScoreSnag</div>
-        <Button sx={{}}>
-          <Link
-            to="/login"
-            style={{
-              textDecoration: "none",
-              color: "inherit",
-              fontFamily: "Poppins",
-              fontSize: "20px",
-              fontWeight: "bold",
-            }}
-          >
-            Login
-          </Link>
-        </Button>
-      </div>
-    )
+        <div className="poppins-bold grow ml-2 text-xl text-primary">
+          ScoreSnag
+        </div>
+      </a>
+      <Button sx={{}}>
+        <Link
+          to="/login"
+          style={{
+            textDecoration: "none",
+            color: "inherit",
+            fontFamily: "Poppins",
+            fontSize: "20px",
+            fontWeight: "bold",
+          }}
+        >
+          Login
+        </Link>
+      </Button>
+    </div>
+  );
 }
 
 export default Navbar;
