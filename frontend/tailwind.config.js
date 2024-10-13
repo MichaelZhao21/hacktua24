fa474f56ef0903bb15f169db5f373b933694e3d8
saj62 <sarannjac@gmail.com>
2024-10-13 03:45:47 -0500

    styled front

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: [
+        '"Poppins", sans-serif', // Change to Poppins
+        {
+          fontFeatureSettings: '"cv11", "ss01"', // Optional: Keep if you need specific features
+          fontVariationSettings: '"opsz" 32' // Optional: Keep if you need specific settings
+        },
+      ],
+    },
+    extend: {
+        colors: {
+            primary: '#7981F3',
+            secondary: '#FFD78B',
+        },
+    },
   },
   plugins: [],
-}
-
+};
